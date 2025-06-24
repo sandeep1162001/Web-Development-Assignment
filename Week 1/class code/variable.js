c1e87f02c6f52ad2996f18435a5fc7de32775adb
sandeep1162001 <156881870+sandeep1162001@users.noreply.github.com>
2025-06-24 15:30:08 +0530

    Create variable.js

---
+++ Week 1/class code/variable.js
@@ -0,0 +1,19 @@
+let firstName = "Sandeep";
+console.log(firstName); //Sandeep
+
+const age = 21;
+console.log(age); // 21
+
+var isStudent = true;
+console.log(isStudent); // true
+
+
+console.log("firstName"); // firstName
+console.log("Sandeep"); // Sandeep
+
+
+isStudent = "Sujeet";
+console.log(isStudent); // Sujeet
+
+isStudent = 10;
+console.log(isStudent); // 10
